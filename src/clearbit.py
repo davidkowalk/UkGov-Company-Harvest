29540945f9ef5a2cfa169f8984d748c0722df6df
DapfiDuck <david.kowalk@gmail.com>
2019-12-28 23:04:02 +0100

    Revert "Added Clearbit Interface"
    This reverts commit 753228665ad45642477e798683976b7ece47aaf3.

--- src/clearbit.py
+++ src/clearbit.py
@@ -1,21 +1,13 @@
 from json import loads as parse
+import webloader
 from urllib.parse import quote_plus as url
 
 def autocomplete(name):
     try:
         http_name = url(name)
-        content = load(f"https://autocomplete.clearbit.com/v1/companies/suggest?query={http_name}")
+        content = webloader.load(f"https://autocomplete.clearbit.com/v1/companies/suggest?query={http_name}")
         json = parse(content)
         return json
     except:
         print("Error")
         return None
-
-# Http Request
-from urllib.request import urlopen as ureq
-
-def load(url):
-    client = ureq(url)
-    content = client.read()
-    client.close()
-    return content
